95dcd69883abdf51fcf489f144ca53a1907e1c1a
Esa-Matti Suuronen <code@esamatti.fi>
2026-05-20 00:23:43 +0300

    Format start.sh mitmweb command for readability
    Also removes the hardcoded web_password option.

--- start.sh
+++ start.sh
@@ -7,4 +7,11 @@ confdir=/home/mitmwall/.mitmproxy
 
 echo "Starting mitmwall..."
 
-exec "$bindir/mitmweb" --set confdir="$confdir" --listen-port 58080 --web-port 58081 --set web_password=secret --mode transparent --showhost -s "$bindir/mitmwall_addon.py" >>"$bindir/logs/mitmweb.log" 2>&1
+exec "$bindir/mitmweb" \
+  --set confdir="$confdir" \
+  --listen-port 58080 \
+  --web-port 58081 \
+  --mode transparent \
+  --showhost \
+  -s "$bindir/mitmwall_addon.py" \
+  >>"$bindir/logs/mitmweb.log" 2>&1
