246bb4f1e4c54d052d8ade27de370eeca3d438d0
Esa-Matti Suuronen <code@esamatti.fi>
2026-05-20 17:24:04 +0300

    Skip service stop in dev-install if unit does not exist

--- dev-install.sh
+++ dev-install.sh
@@ -2,7 +2,10 @@
 
 set -eu
 
-sudo systemctl stop mitmwall.service
+if sudo systemctl list-unit-files mitmwall.service >/dev/null 2>&1; then
+    sudo systemctl stop mitmwall.service
+fi
+
 sudo ./install.sh
 sudo install -m 0644 ./rules.toml /opt/mitmwall/rules.toml
 sudo systemctl restart mitmwall.service
